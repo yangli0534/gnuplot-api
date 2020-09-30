e9b5f6baec5c5a28d79b21b0adab75f4e5818399
yangli0534 <yangli0534@yahoo.com>
2020-09-30 17:19:27 +0800

    update gpib addr

--- station/InstrumentAddrConfig.py
+++ station/InstrumentAddrConfig.py
@@ -6,10 +6,10 @@
 
 """
 
-inst_addr_conf = {'inst_SA':{'cmd_set': 'SA_cmd_set', 'ip_addr': '-', 'GPIB_addr': '20', 'ctrl_mode': 'GPIB'},
-                  'inst_SG':{'cmd_set': 'SG_cmd_set', 'ip_addr': '-', 'GPIB_addr': '21', 'ctrl_mode': 'GPIB'},
-                  'inst_PSU':{'cmd_set': 'PSU_cmd_set', 'ip_addr': '-', 'GPIB_addr': '22', 'ctrl_mode': 'GPIB'},
-                  'inst_PM':{'cmd_set': 'PM_cmd_set', 'ip_addr': '-', 'GPIB_addr': '23', 'ctrl_mode': 'GPIB'},
+inst_addr_conf = {'inst_SA':{'cmd_set': 'SA_cmd_set', 'ip_addr': '-', 'GPIB_addr': '25', 'ctrl_mode': 'GPIB'},
+                  'inst_SG':{'cmd_set': 'SG_cmd_set', 'ip_addr': '-', 'GPIB_addr': '19', 'ctrl_mode': 'GPIB'},
+                  'inst_PSU':{'cmd_set': 'PSU_cmd_set', 'ip_addr': '-', 'GPIB_addr': '06', 'ctrl_mode': 'GPIB'},
+                  'inst_PM':{'cmd_set': 'PM_cmd_set', 'ip_addr': '-', 'GPIB_addr': '13', 'ctrl_mode': 'GPIB'},
                   'inst_OSC':{'cmd_set': 'OSC_cmd_set', 'ip_addr': '-', 'GPIB_addr': '-', 'ctrl_mode': '-'},
                   'inst_SW':{'cmd_set': 'SW_cmd_set', 'ip_addr': '-', 'GPIB_addr': '-', 'ctrl_mode': '-'}
                   }
